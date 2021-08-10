379b03dc09cd6eb918dae65950ac15d97bbe6d42
Josh Stillman <josh.stillman@giantmachines.com>
2021-08-09 18:41:23 -0700

    change title to buwwito

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BUWWITO TIME
         </a>
       </header>
     </div>
